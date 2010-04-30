b17b7321b5b2654127bbde5d22924f04a0f450c1
James Wright <james@chumsley.org>
2010-04-30 14:19:15 -0700

    Implemented a working status buffer for staging/unstaging changes at a file granularity.

--- jgit.el
+++ jgit.el
@@ -1,7 +1,3 @@
-;;TODO
-;; 1. Staging
-;; 2. Commit
-
 ;;; jgit.el --- xgit-like git integration for emacs
 
 ;; Copyright (C) 2010 James Wright
@@ -39,6 +35,15 @@
        (:background "grey85"))
       (t (:bold t)))
   "Common aspects of headers"
+  :group 'git)
+
+(defface git-chapter-header
+    '((((class color) (background dark))
+       (:foreground "yellow"))
+      (((class color) (background light))
+       (:foreground "black" :background "yellow" :bold t))
+      (t (:bold t)))
+  "Face used to highlight stage headers"
   :group 'git)
 
 (defface git-file-header
@@ -149,6 +154,7 @@
     (define-key map [?S] 'git-push)
 ;    (define-key map [?i] 'git-init)
     (define-key map [?m] 'git-query-manifest)
+    (define-key map [?s] 'git-status)
     (define-key map [?w] 'git-whatsnew)
 ;    (define-key map [?x] 'git-remove)
     map)
@@ -159,24 +165,14 @@
 
 (defvar git-hunk-display-map
   (let ((map (make-sparse-keymap)))
-    (define-key map [?\ ] 'git-toggle-hunk-included)
-    (define-key map [?\r] 'git-toggle-hunk-expanded)
+    (define-key map [?\r] 'git-toggle-expanded)
     (define-key map [(control return)] 'git-find-hunk-in-other-window)
     (define-key map [?j] 'git-next-hunk)
     (define-key map [?k] 'git-prev-hunk)
     (define-key map [?J] 'git-next-file)
     (define-key map [?K] 'git-prev-file)
-    (define-key map [?y] 'git-include-hunk)
-    (define-key map [?n] 'git-exclude-hunk)
-    (define-key map [?r] 'git-refine-hunk)
-    (define-key map [?R] 'git-refine-all-hunks)
-    (define-key map [?s] 'git-exclude-remaining-in-file)
-    (define-key map [?f] 'git-include-remaining-in-file)
     (define-key map [?a] 'git-expand-all-hunks)
     (define-key map [?z] 'git-collapse-all-hunks)
-    (define-key map [?Y] 'git-include-all-hunks)
-    (define-key map [?N] 'git-exclude-all-hunks)
-    (define-key map [?d] 'git-exclude-remaining)
     (define-key map [?q] 'darcs-quit-current)
     map)
   "Keymap for displaying lists of atomic hunks")
@@ -186,8 +182,18 @@
     (set-keymap-parent map git-hunk-display-map)
     (define-key map [(control ?c) (control ?s)] 'git-stage-from-whatsnew)
     (define-key map [(control ?c) (control ?c)] 'git-commit-from-whatsnew)
-;    (define-key map [(control ?c) (control ?r)] 'git-commit-revert)
+;    (define-key map [(control ?c) (control ?r)] 'git-revert)
     (define-key map [(control ?x) ?#] 'git-commit-from-whatsnew)
+    (define-key map [?r] 'git-refine-hunk)
+    (define-key map [?R] 'git-refine-all-hunks)
+    (define-key map [?y] 'git-include-hunk)
+    (define-key map [?n] 'git-exclude-hunk)
+    (define-key map [?s] 'git-exclude-remaining-in-file)
+    (define-key map [?f] 'git-include-remaining-in-file)
+    (define-key map [?Y] 'git-include-all-hunks)
+    (define-key map [?N] 'git-exclude-all-hunks)
+    (define-key map [?d] 'git-exclude-remaining)
+    (define-key map [?\ ] 'git-toggle-hunk-included)
     map)
   "Keymap for git-whatsnew-mode")
 
@@ -248,7 +254,14 @@
 
           ((looking-at "deleted file mode \\([0-7]+\\)")
            (setq latest-permissions (match-string 1))
-           (setq delcheated t)
+           (setq delcheated t
+                 newfile nil)
+           (kill-this-line))
+
+          ((looking-at "new file mode \\([0-7]+\\)")
+           (setq latest-permissions (match-string 1))
+           (setq newfile t
+                 delcheated nil)
            (kill-this-line))
 
           ((looking-at "--- \\(?:a/\\([^\r\n]*\\)\\|/dev/null\\)")
@@ -305,7 +318,10 @@
               (xlat-perms (substring permstring 4 5) 2 "s")
               (xlat-perms (substring permstring 5 6) 1 "t")))))
 
-(defvar git-file-header-re "^[ld-][r-][w-][xs-][r-][w-][xs-][r-][w-][xt-] \\([^:]+\\):?  ?\\(.*\\)\n"
+(defvar git-chapter-header-re "^\t:\\(Staged\\|Unstaged\\|Untracked\\):\n"
+  "Regular expression for post-formatting status 'chapter' headers")
+
+(defvar git-file-header-re "^[ld-][r-][w-][xs-][r-][w-][xs-][r-][w-][xt-] \\([^:]+\\):?  ?\\([^\r\n]*\\)[\r\n]"
   "Regular expression for post-formatting file headers.")
 
 (defvar git-hunk-header-re "\\(^@@ -\\(?:[0-9]+\\)\\(?:,\\(?:[0-9]+\\)\\)? \\+\\([0-9]+\\)\\(?:,\\(?:[0-9]+\\)\\)? @@\\).*$"
@@ -347,6 +363,12 @@
   (setq selective-display t)
   (font-lock-fontify-buffer))
 
+(defun git-chapter-header-p ()
+  "Non-nil if point is currently on a chapter header line"
+  (save-excursion
+    (goto-char (point-at-bol))
+    (looking-at git-chapter-header-re)))
+
 (defun git-file-header-p ()
   "Non-nil if point is currently on a file header line"
   (save-excursion
@@ -364,7 +386,7 @@
   (if (git-file-header-p)
     (point-at-bol)
     (save-excursion
-      (if (re-search-backward git-file-header-re)
+      (if (re-search-backward git-file-header-re nil t)
         (point)
         (error "No current file")))))
 
@@ -373,10 +395,12 @@
   (save-excursion
     (goto-char (git-file-beginning))
     (while (and (zerop (forward-line 1))
-                (not (git-file-header-p))))
-      (when (= (point) (point-at-bol))
-        (forward-line -1))
-      (point-at-eol)))
+                (not (git-file-header-p))
+                (not (git-chapter-header-p))
+                (not (looking-at "^$"))))
+    (when (= (point) (point-at-bol))
+      (forward-line -1))
+    (point-at-eol)))
 
 (defun git-hunk-beginning (&optional noerror)
   "Returns the position of the beginning of the current hunk"
@@ -396,10 +420,12 @@
     (goto-char (git-hunk-beginning))
     (while (and (zerop (forward-line 1))
                 (not (git-file-header-p))
-                (not (git-hunk-header-p))))
-      (when (= (point) (point-at-bol))
-        (forward-line -1))
-      (point-at-eol)))
+                (not (git-hunk-header-p))
+                (not (looking-at "^[^\\ +-]"))
+                (not (looking-at "^$"))))
+    (when (= (point) (point-at-bol))
+      (forward-line -1))
+    (point-at-eol)))
 
 (defun git-next-hunk (&optional noerror)
   "Move point to the beginning of the next hunk"
@@ -458,6 +484,16 @@
       (goto-char p)
       (error "No more files"))))
 
+(defun git-expand-file ()
+  "Expand the current file"
+  (let ((inhibit-read-only t))
+    (subst-char-in-region (git-file-beginning) (git-file-end) ?\^M ?\n)))
+
+(defun git-collapse-file ()
+  "Collapse the current file"
+  (let ((inhibit-read-only t))
+    (subst-char-in-region (git-file-beginning) (git-file-end) ?\n ?\^M)))
+
 (defun git-expand-hunk ()
   "Expand the current hunk"
   (let ((inhibit-read-only t))
@@ -474,12 +510,24 @@
     (goto-char (git-hunk-beginning))
     (re-search-forward "\n" (git-hunk-end) t)))
 
-(defun git-toggle-hunk-expanded ()
+(defun git-file-expanded-p ()
+  "Return non-NIL if the current file is expanded"
+  (save-excursion
+    (goto-char (git-file-beginning))
+    (re-search-forward "\n" (git-file-end) t)))
+
+(defun git-toggle-expanded ()
   "Expand or collapse the current hunk"
   (interactive)
-  (if (git-hunk-expanded-p)
-      (git-collapse-hunk)
-      (git-expand-hunk)))
+  (cond
+    ((and (git-file-header-p)
+          (git-file-expanded-p))
+     (git-collapse-file))
+    ((git-file-header-p)
+     (git-expand-file))
+    ((git-hunk-expanded-p)
+     (git-collapse-hunk))
+    (t (git-expand-hunk))))
 
 (defvar *git-current-filename* nil
   "Contains a record describing filename of the current enclosing
@@ -527,6 +575,8 @@
 
 (defun git-hunk-plist ()
   "Returns a plist describing the current hunk"
+  (unless (git-hunk-beginning)
+    (error "No current hunk"))
   (let ((filename (git-current-filename))
         (hunkid (save-excursion
                   (goto-char (git-hunk-beginning))
@@ -719,19 +769,142 @@
 (defun git-status (&optional same-window)
   "Gives the current status of the index and working tree."
   (interactive)
-  (let ((repo-dir default-directory)) ;TODO `git-repo-dir'
-    (if same-window
-      (switch-to-buffer (format "*git status: (%s)*" repo-dir))
-      (switch-to-buffer-other-window (format "*git status: (%s)*" repo-dir)))
-    (toggle-read-only 1)
-    (setq default-directory repo-dir)
-    (let ((inhibit-read-only t))
-      (erase-buffer))
-
+  (git-command-window 'status same-window)
+  (let ((inhibit-read-only t))
     ;; Fetch the raw text
     (call-process "git" nil (current-buffer) nil "status")
+    (goto-char (point-min))
+    (git-markup-status)
+    (git-status-mode)
+    (git-on-all-hunks 'git-collapse-file)
     (goto-char (point-min))))
 
+(defun git-current-chapter ()
+  "Returns one of '(staged unstaged untracked) representing the current chapter for point."
+  (save-excursion
+    (if (git-chapter-header-p)
+      (goto-char (point-at-bol))
+      (or (re-search-backward git-chapter-header-re nil t)
+          (error "No current chapter")))
+    (looking-at git-chapter-header-re)
+    (let ((h (match-string 1)))
+      (cond
+        ((string= h "Staged") 'staged)
+        ((string= h "Unstaged") 'unstaged)
+        ((string= h "Untracked") 'untracked)))))
+
+(defun git-toggle-file-staged ()
+  "If the file is currently staged, unstage it.  Otherwise, stage it."
+  (interactive)
+  (let ((chapter (git-current-chapter))
+        (filename (or (git-current-filename) (error "Cannot find current filename"))))
+    (ecase chapter
+      (staged
+       (message "git reset HEAD %s" filename)
+       (message (shell-command-to-string (format "git reset HEAD %s" filename))))
+      ((unstaged untracked)
+       (message "git add %s" filename)
+       (message (shell-command-to-string (format "git add %s" filename)))))
+    (revert-buffer t t t)))
+
+(defvar git-status-font-lock-keywords
+  (append `((,git-chapter-header-re (0 'git-chapter-header)))
+          git-whatsnew-font-lock-keywords))
+
+(defvar git-status-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map git-hunk-display-map)
+    (define-key map [?\ ] 'git-toggle-file-staged)
+    map))
+
+(defun git-status-mode ()
+  (unless (eq major-mode 'git-status)
+    (kill-all-local-variables))
+  (setq font-lock-defaults '((git-status-font-lock-keywords) t))
+  (setq major-mode 'git-status)
+  (setq mode-name "git-status")
+  (use-local-map git-status-map)
+  (set (make-local-variable 'revert-buffer-function)
+       (lambda (ignore-auto noconfirm)
+         (git-status t)))
+  (setq selective-display t)
+  (font-lock-fontify-buffer))
+
+(defun git-markup-status ()
+  "Converts status output to something usable"
+  (let ((lines-left 0)
+        (branch nil)
+        (chapter nil)
+        (e (make-marker)))
+    (flet ((kill-this-line ()
+             (delete-region (point-at-bol)
+                            (save-excursion (setq lines-left (forward-line 1)) (point)))))    
+      (while (zerop lines-left)
+        (setq lines-left nil)
+        (cond
+          ;; Look for the branch-description line
+          ((looking-at "# On branch \\(.*\\)")
+           (setq branch (match-string 1))
+           (kill-this-line))
+
+          ;; Watch for changes in chapter
+          ((looking-at "# Changes to be committed:")
+           (setq chapter 'staged)
+           (kill-this-line)
+           (insert "\n\t:Staged:\n\n"))
+          ((looking-at "# Changed but not updated:")
+           (setq chapter 'unstaged)
+           (kill-this-line)
+           (insert "\n\t:Unstaged:\n\n"))
+          ((looking-at "# Untracked files:")
+           (setq chapter 'untracked)
+           (kill-this-line)
+           (insert "\n\t:Untracked:\n\n"))
+          
+          ;; Add actual files
+          ((looking-at "#\t\\(new file:\\|deleted:\\|modified:\\)?[ ]+\\(.*\\)$")
+           (let ((filename (match-string 2))
+                 (s nil))
+             (kill-this-line)
+             (setq s (point))
+             (ecase chapter
+               (staged (message "git diff --cached %s" filename)
+                       (call-process "git" nil (current-buffer) nil "diff" "--cached" filename))
+               (unstaged (message "git diff %s" filename)
+                       (call-process "git" nil (current-buffer) nil "diff" filename)))
+             (set-marker e (point))
+             (save-restriction
+               (narrow-to-region s e)
+               (goto-char (point-min))
+               (git-markup-hunks))
+             (goto-char e)
+             (message nil)))
+
+          ((and (eq chapter 'untracked)
+                (looking-at "#\t\\(.*\\)$"))
+           (let ((filename (match-string 1))
+                 (s nil))
+             (kill-this-line)
+             (setq s (point))
+             (message "git diff %s" filename)
+             (call-process "git" nil (current-buffer) nil "diff" "/dev/null" filename)
+             (set-marker e (point))
+             (save-restriction
+               (narrow-to-region s e)
+               (goto-char (point-min))
+               (git-markup-hunks))
+             (goto-char e)
+             (message nil)))
+          
+          ;; Kill anything we don't recognize
+          (t
+
+           (kill-this-line)))
+        (unless lines-left
+          (setq lines-left (forward-line 1)))))))
+               
+
+           
 
 ;;;; ------------------------------------- git-commit ------------------------------------
 
@@ -739,19 +912,13 @@
 (defun git-commit (&optional same-window)
   "Commit the currently staged patches."
   (interactive)
-  (let ((repo-dir default-directory)  ;TODO `git-repo-dir'
-        (inhibit-read-only t))
-    (if same-window
-      (switch-to-buffer (format "*git commit: (%s)*" repo-dir))
-      (switch-to-buffer-other-window (format "*git commit: (%s)*" repo-dir)))
-    (setq default-directory repo-dir)
-    (erase-buffer)
-    (insert (format (substitute-command-keys git-commit-buffer-instructions)
-                    (git-current-branch)))
-    (git-hunks repo-dir '("diff" "--cached") nil
-               (lambda (str)
-                 (save-excursion (insert str))
-                 (git-markup-hunks)))))
+  (git-command-window 'commit same-window)
+  (insert (format (substitute-command-keys git-commit-buffer-instructions)
+                  (git-current-branch)))
+  (git-hunks '("diff" "--cached") nil
+             (lambda (str)
+               (save-excursion (insert str))
+               (git-markup-hunks))))
 
 ;;;; ====================================== git process interaction =====================================
 
